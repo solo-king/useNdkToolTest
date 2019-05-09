677129697944cf4d986ba9c0a76d72683004048a
chenqigan <836190520@qq.com>
2019-05-09 15:32:17 +0800

    添加可执行文件使用自编译动态库例子

---
+++ cpp/printMyLibVersionExecute/printLibVer.cpp
@@ -0,0 +1,13 @@
+#include"../include/myTestLib.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+    string ver(getVerson());
+    printVerson();
+    return 0;
+}
+
