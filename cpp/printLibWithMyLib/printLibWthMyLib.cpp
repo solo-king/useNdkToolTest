ed0f5f3ef9c0c58bb9e65de13a7d55aa6e2fb137
chenqigan <836190520@qq.com>
2019-05-09 15:50:26 +0800

    1.添加库依赖库例子 2.添加库依赖库使用例子

---
+++ cpp/printLibWithMyLib/printLibWthMyLib.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include"../include/myTestLib.h"
+#include"../include/printLibWthMyLib.h"
+
+using namespace std;
+
+void printLibWithMyLib(){
+
+    printVerson();
+
+}
+    
+
---
+++ cpp/printMyLibVersionExecute2th/printLibVer2th.cpp
@@ -0,0 +1,13 @@
+#include"../include/myTestLib.h"
+#include"../include/printLibWthMyLib.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+    printLibWithMyLib();
+    return 0;
+}
+
